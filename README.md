fe7ace5d75802e6015b5d46476d9da9bec36a8c9
SrinivasScripts <avula0605@gmail.com>
2023-12-20 23:52:15 +0530

    changes,README,Service,Disk

--- README.md
+++ README.md
@@ -1,10 +1,54 @@
-# scripts
-This Bash script monitors CPU and memory usage, services on a Linux system.
+# System Monitoring Script
+
+This Bash script monitors CPU, memory, disk usage, and the status of specified services. It logs relevant information to a file and can be used for system health monitoring.
 
 ## Usage
 
-Simply run the script using the following command:
+1. **Clone the Repository:**
 
-```bash
-./cpumetrics.sh
-./services.sh
+   ```bash
+   git clone https://github.com/SrinivasScripts/scripts.git
+   cd your-repository
+
+Make the Script Executable:
+chmod +x monitor_script.sh
+
+Run the Script:
+./monitor_script.sh
+
+Configuration
+Tailor the script to your specific needs by adjusting the following threshold parameters:
+# Set thresholds for system metrics
+CPU_THRESHOLD=80
+MEMORY_THRESHOLD=75
+DISK_THRESHOLD=80
+Configuration
+Tailor the script to your specific needs by adjusting the following threshold parameters
+
+Features
+Monitors CPU, memory, and disk usage.
+Checks the status of specified services (e.g., SSH, Nginx, Apache2).
+Logs information to a file (cpumetrics.txt by default).
+Retrieves and logs system logs for troubleshooting.
+
+Log File
+The script generates a log file (cpumetrics.txt) containing detailed information on CPU usage, memory consumption, disk utilization, service statuses, and system logs.
+
+To review the last 10 lines of the log file:
+
+tail -n 10 cpumetrics.txt
+
+Customization
+Service Monitoring
+Customize the list of monitored services by modifying the services array in the script:
+services=("ssh" "nginx" "apache2")
+Add or remove services to align with your specific environment.
+
+Log Extraction
+Adjust the NUM_LINES variable in the extract_logs function to specify the number of lines extracted from system logs:
+NUM_LINES=10
+
+License
+This script is licensed under the MIT License - refer to the LICENSE file for details.
+
+Feel free to copy and paste this content into your README.md file.
--- cpumetrics.sh
+++ cpumetrics.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x
+#set -x
 # This script monitors CPU and memory usage
 
 # Set thresholds for system metrics
@@ -8,14 +8,15 @@ MEMORY_THRESHOLD=75
 DISK_THRESHOLD=80
 # Log file
 LOG_FILE="cpumetrics.txt"
+services=("ssh" "nginx" "apache2")
 
 # Function to check CPU and memory usage
 check_usage() {
   # Get the current usage of CPU and memory
   cpuUsage=$(mpstat 1 1 | awk '/Average:/ { print 100 - $NF}')
   memUsage=$(free -m | awk '/Mem/{print $3}')
-  DISK_USAGE=$(df -h / | awk '/\// {print $(NF-1)}' | sed 's/%//')
-   
+  diskUsage=$(df -h / | awk '/\// {print $(NF-1)}' | sed 's/%//')
+
 # Print the usage
   echo "CPU Usage: $cpuUsage%"
   echo "Memory Usage: $memUsage MB"
@@ -24,8 +25,8 @@ check_usage() {
     if [ -z "$cpuUsage" ] || [ "$cpuUsage" -gt "$CPU_THRESHOLD" ]; then
         echo "ERROR: CPU Usage exceeds $CPU_THRESHOLD%: ${cpuUsage:-N/A}" >> "$LOG_FILE"
         return 1
-else 
-	echo "CPU Usage ${cpuUsage}" >> "$LOG_FILE"
+else
+        echo "CPU Usage ${cpuUsage}" >> "$LOG_FILE"
     fi
 
     # Check memory threshold
@@ -33,34 +34,39 @@ else
         echo "ERROR: Memory Usage exceeds $MEMORY_THRESHOLD%: ${memUsage:-N/A}" >> "$LOG_FILE"
         return 1
 else
-	echo "Memory Usage ${memUsage}" >> "$LOG_FILE"
+        echo "Memory Usage ${memUsage}" >> "$LOG_FILE"
     fi
       # Check disk threshold
-    if [ -z "$diskUsage" ] ||["$diskUsage" -gt "$DISK_THRESHOLD" ]; then
+    if [ -z "$diskUsage" ] ||["$diskUsage" -gt "$DISK_THRESHOLD"]; then
         echo "Disk Usage exceeds $DISK_THRESHOLD%: $diskUsage%" >> "$LOG_FILE"
-	return 1
+        return 1
 else
-	echo "Disk Usage ${diskUsage}" >> "$LOG_FILE"
-        
+        echo "Disk Usage ${diskUsage}" >> "$LOG_FILE"
+
     fi
 }
 # Function to check service status
 check_service() {
     local SERVICE=$1
+
     if [ -z "$SERVICE" ]; then
         echo "Error: No service specified for checking."
         return 1
     fi
 
-    local STATUS="$(systemctl is-active $SERVICE)"
+    local STATUS=$(systemctl is-active $SERVICE)
 
     if [ "${STATUS}" = "active" ]; then
-        echo "$(date) - $SERVICE Service is Active" >> "$LOG_FILE"
+        echo "$SERVICE is active."
     else
-        echo "$(date) - ERROR: $SERVICE Service not Active" >> "$LOG_FILE"
-        return 1
+        echo "$SERVICE is not active."
     fi
 }
+
+# Check the status of each service in the list
+for service in "${services[@]}"; do
+    check_service "$service"
+done
 extract_logs() {
     LOG_FILE="/var/log/syslog"
     NUM_LINES=10
@@ -80,21 +86,20 @@ extract_logs() {
     # Redirect standard output and standard error to metrics.txt
 
         #exec > cpumetrics.txt 2>&1
-	   # logs=$(extract_logs)
-	#echo "Monitoring Script - $(date)" >> "$LOG_FILE"
+           # logs=$(extract_logs)
+        #echo "Monitoring Script - $(date)" >> "$LOG_FILE"
         #echo "Timestamp: $(date)" >> "$LOG_FILE"
-	#echo "$logs" >> "$LOG_FILE"
+        #echo "$logs" >> "$LOG_FILE"
         check_usage
-	check_service "plymouth"
-	logs=$(extract_logs)
+        check_service
+        logs=$(extract_logs)
         echo "Monitoring Script - $(date)" >> "$LOG_FILE"
         #echo "Timestamp: $(date)" >> "$LOG_FILE"
         echo "$logs" >> "$LOG_FILE"
-   
+
 
     echo "Metrics collected successfully."
 }
 
 # Run the main function
 main
-
